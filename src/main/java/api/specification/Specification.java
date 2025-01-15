2ccb869691249ae49b9de229564333ea2ff47403
Stas_Makarov <titanstas@yandex.ru>
2025-01-15 18:35:40 +0300

    Add javadoc for spec

--- src/main/java/api/specification/Specification.java
+++ src/main/java/api/specification/Specification.java
@@ -7,33 +7,45 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class Specification {
-
+    /**
+     * Метод для добавления спецификации запроса
+     */
     public static RequestSpecification requestSpec() {
         return  new RequestSpecBuilder()
                 .setBaseUri("https://api.sapling.ai/")
                 .setContentType("application/json")
                 .build();
     }
-
+    /**
+     * Метод для добавления спецификации ответа 200
+     */
     public static ResponseSpecification responseSpec200(){
         return new ResponseSpecBuilder()
                 .expectStatusCode(200)
                 .build();
     }
-
+    /**
+     * Метод для установки спецификации запроса
+     */
     public static void installSpec(RequestSpecification requestSpec){
         RestAssured.requestSpecification = requestSpec;
     }
-
+    /**
+     * Метод для установки спецификации ответа
+     */
     public static void installSpec(ResponseSpecification responseSpec){
         RestAssured.responseSpecification = responseSpec;
     }
-
+    /**
+     * Метод для установки спецификации запроса и ответа
+     */
     public static void installSpec(RequestSpecification requestSpec, ResponseSpecification responseSpec){
         RestAssured.requestSpecification = requestSpec;
         RestAssured.responseSpecification = responseSpec;
     }
-
+    /**
+     * Метод для удаления спецификации
+     */
     public static void deleteSpec() {
         RestAssured.requestSpecification = null;
         RestAssured.responseSpecification = null;
